4c8210d7699b8976e92d265922fb7d0767c80133
Andy Muniz <munizandy94@gmail.com>
2020-07-07 23:06:48 -0500

    Added jest for tests. Mul

--- multiplePointersChallenge.js
+++ multiplePointersChallenge.js
@@ -19,7 +19,4 @@ function countUniqueValues(array) {
     return i + 1;
 }
 
-console.log(countUniqueValues([1,1,1,1,1,2]))
-console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]))
-console.log(countUniqueValues([]))
-console.log(countUniqueValues([-2,-1,-1,0,1]))
+module.exports = countUniqueValues;
---
+++ multiplePointersChallenge.test.js
@@ -0,0 +1,9 @@
+const countUniqueValues = require('./multiplePointersChallenge');
+
+test('count unique values in array', () => {
+    expect(countUniqueValues([1, 1, 1, 1, 1, 2])).toBe(2)
+    expect(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])).toBe(7)
+    expect(countUniqueValues([])).toBe(0)
+    expect(countUniqueValues([-2, -1, -1, 0, 1])).toBe(4)
+})
+
