ba9e48e33b1c44dc2be1dbff8de18744f4b67277
= <=>
2020-07-13 15:46:07 -0500

    Recursion Challenges Pt 1

--- recursionChallenge.js
+++ recursionChallenge.js
@@ -4,15 +4,40 @@ function reverse(str) {
 }
 
 function isPalindrome(str) {
-
+    if(str.length === 1) return true;
+    if(str[0] !== str[str.length - 1])
+        return false;
+    let subArray = str.substring(1, str.length - 1);
+    return isPalindrome(subArray);
 }
 
-function someRecursive() {
-
+/**
+ * Accepts array and a callback
+ * Returns true if a single value in the array returns true when passed to callback.
+ * Otherwise returns false
+ */
+function isOdd(val) {
+    return val % 2 !== 0;
+}
+function someRecursive(array, callback) {
+    if(array.length === 0) return false;
+    if(callback(array[0])) return true;
+    return someRecursive(array.slice(1), callback);
 }
 
-function flatten () {
-
+/**
+ * Accepts an array of arrays.
+ * Returns a new array with all values flattened.
+ */
+function flatten (array) {
+    if(array.length === 0) return [];
+    let value = array[0];
+    if(Array.isArray(value)){
+        return flatten(value).concat(flatten(array.slice(1)))
+    }
+    else {
+        return [array[0]].concat(flatten(array.slice(1)))
+    }
 }
 
 module.exports = {
--- tests/recursionChallenge.test.js
+++ tests/recursionChallenge.test.js
@@ -14,10 +14,19 @@ test('should isPalindrome', () => {
     expect(isPalindrome('amanaplanacanalpandemonium')).toBe(false)
 })
 
+function isOdd(val) {
+    return val % 2 !== 0;
+}
 test('should someRecursive', () => {
-
+    expect(someRecursive([1, 2, 3, 4], isOdd)).toBe(true)
+    expect(someRecursive([4, 6, 8, 9], isOdd)).toBe(true)
+    expect(someRecursive([4, 6, 8], isOdd)).toBe(false)
+    expect(someRecursive([4, 6, 8], val => val > 10)).toBe(false)
 })
 
 test('should flatten', () => {
-
+    expect(flatten([1,2,3,[4,5]])).toStrictEqual([1,2,3,4,5])
+    expect(flatten([1,[2, [3, 4], [[5]]]])).toStrictEqual([1,2,3,4,5])
+    expect(flatten([[1], [2], [3]])).toStrictEqual([1,2,3])
+    expect(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])).toStrictEqual([1,2,3])
 })
