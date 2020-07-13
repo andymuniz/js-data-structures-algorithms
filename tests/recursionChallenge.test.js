6cd16c0bb08e0019125a8e3c5d583d3c8bab99e7
= <=>
2020-07-13 17:38:24 -0500

    Recursion challenge pt 2

--- tests/recursionChallenge.test.js
+++ tests/recursionChallenge.test.js
@@ -1,5 +1,5 @@
 const recursionChallenge = require('../recursionChallenge')
-const {reverse, isPalindrome, someRecursive, flatten } = recursionChallenge;
+const { reverse, isPalindrome, someRecursive, flatten, capitalizeFirst, nestedEvenSum, capitalizeWords, stringifyNumbers, collectStrings } = recursionChallenge;
 
 test('reverse', () => {
     expect(reverse('awesome')).toBe('emosewa')
@@ -25,8 +25,88 @@ test('should someRecursive', () => {
 })
 
 test('should flatten', () => {
-    expect(flatten([1,2,3,[4,5]])).toStrictEqual([1,2,3,4,5])
-    expect(flatten([1,[2, [3, 4], [[5]]]])).toStrictEqual([1,2,3,4,5])
-    expect(flatten([[1], [2], [3]])).toStrictEqual([1,2,3])
-    expect(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])).toStrictEqual([1,2,3])
+    expect(flatten([1, 2, 3, [4, 5]])).toStrictEqual([1, 2, 3, 4, 5])
+    expect(flatten([1, [2, [3, 4], [[5]]]])).toStrictEqual([1, 2, 3, 4, 5])
+    expect(flatten([[1], [2], [3]])).toStrictEqual([1, 2, 3])
+    expect(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])).toStrictEqual([1, 2, 3])
+})
+
+test('should capitalizeFirst', () => {
+    expect(capitalizeFirst(['car', 'taco', 'banana'])).toStrictEqual(['Car', 'Taco', 'Banana'])
+})
+
+test('should nestedEvenSum', () => {
+    var obj1 = {
+        outer: 2,
+        obj: {
+            inner: 2,
+            otherObj: {
+                superInner: 2,
+                notANumber: true,
+                alsoNotANumber: "yup"
+            }
+        }
+    }
+
+    var obj2 = {
+        a: 2,
+        b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+        c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+        d: 1,
+        e: { e: { e: 2 }, ee: 'car' }
+    };
+
+    expect(nestedEvenSum(obj1)).toBe(6)
+    expect(nestedEvenSum(obj2)).toBe(10)
+})
+
+test('should capitalizeWords', () => {
+    expect(capitalizeWords(['i', 'am', 'learning', 'recursion'])).toStrictEqual(['I', 'AM', 'LEARNING', 'RECURSION'])
+})
+
+test('should stringifyNumbers', () => {
+    let obj = {
+        num: 1,
+        test: [],
+        data: {
+            val: 4,
+            info: {
+                isRight: true,
+                random: 66
+            }
+        }
+    }
+    expect(stringifyNumbers(obj)).toStrictEqual(
+        {
+            num: "1",
+            test: [],
+            data: {
+                val: "4",
+                info: {
+                    isRight: true,
+                    random: "66"
+                }
+            }
+        }
+    )
+})
+
+test('should collectStrings', () => {
+    const obj = {
+        stuff: "foo",
+        data: {
+            val: {
+                thing: {
+                    info: "bar",
+                    moreInfo: {
+                        evenMoreInfo: {
+                            weMadeIt: "baz"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    expect(collectStrings(obj)).toStrictEqual(["foo", "bar", "baz"])
 })
