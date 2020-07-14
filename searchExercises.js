e3255d22eecfef2a2ec50fa5ee70208d78097291
= <=>
2020-07-13 22:20:46 -0500

    Linear Search + Binary Search Exercise

---
+++ searchExercises.js
@@ -0,0 +1,32 @@
+function linearSearch(array, searchVal) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === searchVal) return i;
+    }
+    return -1;
+}
+
+/**
+ * Assume array is sorted.
+ */
+function binarySearch(array, searchVal) {
+    let start = 0;
+    let end = array.length - 1;
+    while(end > start) {
+        let pivot = end + start / 2;
+        let pivotVal = array[pivot];
+        if(pivotVal === searchVal)
+            return pivot;
+        else if (searchVal < pivotVal) {
+            end = pivot - 1;
+        }
+        else {
+            start = pivot + 1;
+        }
+    }
+    return -1;
+}
+
+module.exports = {
+    linearSearch,
+    binarySearch
+}
---
+++ tests/searchExercises.test.js
@@ -0,0 +1,21 @@
+const { linearSearch, binarySearch } = require('../searchExercises')
+
+test('should linearSearch', () => {
+    expect(linearSearch([10, 15, 20, 25, 30], 15)).toBe(1)
+    expect(linearSearch([9, 8 , 7, 6, 5, 4, 3, 2, 1, 0], 4)).toBe(5)
+    expect(linearSearch([100], 100)).toBe(0)
+    expect(linearSearch([1,2,3,4,5], 6)).toBe(-1)
+    expect(linearSearch([9,8,7,6,5,4,3,2,1,0], 10)).toBe(-1)
+    expect(linearSearch([100], 200)).toBe(-1)
+})
+
+test('should binarySearch', () => {
+    expect(binarySearch([1,2,3,4,5], 2)).toBe(1)
+    expect(binarySearch([1,2,3,4,5], 3)).toBe(2)
+    expect(binarySearch([1,2,3,4,5], 5)).toBe(4)
+    expect(binarySearch([1,2,3,4,5], 6)).toBe(-1)
+    expect(binarySearch([5,6,10,13,14,18,30,34,35,37,40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10)).toBe(2)
+    expect(binarySearch([5,6,10,13,14,18,30,34,35,37,40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95)).toBe(16)
+    expect(binarySearch([5,6,10,13,14,18,30,34,35,37,40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 100)).toBe(-1)
+})
+
