35d35db95c4a952bcbc2d441016783d340bb7618
Andy Muniz <munizandy94@gmail.com>
2020-07-13 22:41:19 -0500

    Cleanup binary search.

--- searchExercises.js
+++ searchExercises.js
@@ -1,6 +1,6 @@
 function linearSearch(array, searchVal) {
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === searchVal) return i;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === searchVal) return i;
     }
     return -1;
 }
@@ -11,17 +11,14 @@ function linearSearch(array, searchVal) {
 function binarySearch(array, searchVal) {
     let start = 0;
     let end = array.length - 1;
-    while(end >= start) {
+    while (end >= start) {
         let pivot = Math.floor((end + start) / 2);
-        let pivotVal = array[pivot];
-        if(pivotVal === searchVal)
+        if (array[pivot] === searchVal)
             return pivot;
-        else if (searchVal < pivotVal) {
+        else if (searchVal < array[pivot])
             end = pivot - 1;
-        }
-        else {
+        else
             start = pivot + 1;
-        }
     }
     return -1;
 }
