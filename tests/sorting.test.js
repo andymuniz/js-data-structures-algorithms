cf47e85ce7711ce2b2ddc93341973533d6c61e19
Andy Muniz <munizandy94@gmail.com>
2020-07-15 19:06:48 -0500

    Insertion Sort

--- tests/sorting.test.js
+++ tests/sorting.test.js
@@ -1,4 +1,4 @@
-const { bubbleSort, selectionSort } = require('../sorting')
+const { bubbleSort, selectionSort, insertionSort } = require('../sorting')
 
 test('should bubbleSort', () => {
     expect(bubbleSort([0, 1, 2])).toStrictEqual([0, 1, 2])
@@ -11,4 +11,11 @@ test('should selectionSort', () => {
     expect(selectionSort([1, 2, 0])).toStrictEqual([0, 1, 2])
     expect(selectionSort([37, 45, 29, 8])).toStrictEqual([8, 29, 37, 45])
     expect(selectionSort([0, 2, 34, 22, 10, 19, 17])).toStrictEqual([0, 2, 10, 17, 19, 22, 34])
+})
+
+test('should insertionSort', () => {
+    expect(insertionSort([0, 1, 2])).toStrictEqual([0, 1, 2])
+    expect(insertionSort([1, 2, 0])).toStrictEqual([0, 1, 2])
+    expect(insertionSort([37, 45, 29, 8])).toStrictEqual([8, 29, 37, 45])
+    expect(insertionSort([0, 2, 34, 22, 10, 19, 17])).toStrictEqual([0, 2, 10, 17, 19, 22, 34])
 })
